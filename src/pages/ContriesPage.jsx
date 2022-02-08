c979da2d47fdb2ea4b2820df1589de6e7fcff6ba
rafonha <johndoe@example.comrafaela.sdp25@gmail.com>
2022-02-08 20:49:23 -0300

    create country input

--- src/pages/ContriesPage.jsx
+++ src/pages/ContriesPage.jsx
@@ -1,7 +1,18 @@
 import Header from '../components/Header'
 import Main from '../components/Main'
+import GenericInput from '../components/GenericInput'
+import { allCountries } from '../data/countries';
+import { useState } from 'react';
 
 export default function ContriesPage() {
+  const [countryFilter, setcountryFilter] = useState("Brazil");
+
+  function handleCountryFilter(newCountryFilter) {
+    
+    console.log("newCountryFilter", newCountryFilter)
+    setcountryFilter(newCountryFilter)
+  }
+
   return(
       <>
         <Header
@@ -9,7 +20,17 @@ export default function ContriesPage() {
           headerClass={'bg-yellow-300 mx-auto  p-4'}
         />
         <Main>
-          
+          <GenericInput
+            inputLabel = 'Nome do país (pelo menos 3 caracteres): ' 
+            name='inputCountryName'
+            type='text'
+            id='inputCountryName' 
+            inputClass='border' 
+            onInputChange={handleCountryFilter}
+            inputValue={countryFilter}
+            autoFocus
+            divClass=''
+          />
         </Main>
       </>
   );
